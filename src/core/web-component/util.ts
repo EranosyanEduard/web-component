ed4456d2a8f08cb3a5324956c2fb08c756d5b943
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2022-10-07 10:49:53 +0300

    fix: добавить импорт утилиты show

--- src/core/web-component/util.ts
+++ src/core/web-component/util.ts
@@ -1,4 +1,4 @@
-import { map, when } from './template'
+import { map, show, when } from './template'
 import { defineComponents } from './utils'
 
-export { defineComponents, map, when }
+export { defineComponents, map, show, when }
